b814c50cab4b5c511f98953f352e0a4274ae5604
danielfalqui <100230158+danielfalqui@users.noreply.github.com>
2023-02-20 13:39:07 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+import firebaseConfig = {
+    apiKey: "AIzaSyBqFzQscgC3PpJudnyNYilLOKZ8CIYzmgQ",
+    authDomain: "aplicativo-espectograma-dc761.firebaseapp.com",
+    projectId: "aplicativo-espectograma-dc761",
+    storageBucket: "aplicativo-espectograma-dc761.appspot.com",
+    messagingSenderId: "309052907708",
+    appId: "1:309052907708:web:f08fa2124ad12d65c070b9",
+    measurementId: "G-9EY884PXDS"
+  };
+  
